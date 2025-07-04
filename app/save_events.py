a60fb9f1669a4de0a7b7b364f9fd3a327d132f81
Alvaro González Gallardo <mscinf12@masaveu.net>
2025-07-04 14:32:40 +0200

    Añadir todos los eventos para probar

--- app/save_events.py
+++ app/save_events.py
@@ -30,12 +30,12 @@ def guardar_eventos(scrapers=None):
     if scrapers is None:
         from app.script_scraping import get_events_gijon, get_events_oviedo, get_events_mieres, get_events_asturiescultura, get_events_aviles, get_events_siero, get_events_conciertosclub
         scrapers = [
-            #get_events_gijon, 
-            #get_events_oviedo, 
-            #get_events_mieres,
-            #get_events_asturiescultura,
-            #get_events_aviles,
-            #get_events_siero,
+            get_events_gijon, 
+            get_events_oviedo, 
+            get_events_mieres,
+            get_events_asturiescultura,
+            get_events_aviles,
+            get_events_siero,
             get_events_conciertosclub
             ]
 
