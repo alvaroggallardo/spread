45728e346e3a938cb56dad604251b19769f21e70
Alvaro González Gallardo <mscinf12@masaveu.net>
2025-07-04 11:25:24 +0200

    Añadir página asturiescultura_2

--- app/save_events.py
+++ app/save_events.py
@@ -28,7 +28,7 @@ def guardar_eventos(scrapers=None):
     db = SessionLocal()
 
     if scrapers is None:
-        from app.script_scraping import get_events_gijon, get_events_oviedo, get_events_mieres, , get_events_asturiescultura
+        from app.script_scraping import get_events_gijon, get_events_oviedo, get_events_mieres, get_events_asturiescultura
         scrapers = [
             #get_events_gijon, 
             #get_events_oviedo, 
